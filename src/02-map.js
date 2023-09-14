ba3ea9ab455c93e727126cc892b37cb6008a5de3
juligarc91 <julissagarcia@pursuit.org>
2023-09-14 19:05:56 -0400

    Modded 6 .js files from src directory

--- src/02-map.js
+++ src/02-map.js
@@ -13,7 +13,10 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {string[]} An array of strings, all of which are song titles.
  */
-function getSongTitles(songs) {}
+function getSongTitles(songs) {
+  const songTitles = songs.map((song) => song.title);
+    return songTitles;
+};
 
 /**
  * Returns an array of all of the song titles with the artist.
@@ -24,7 +27,10 @@ function getSongTitles(songs) {}
  *  getSongDetails(songs);
  *  //> [ "Berlin Tsukin by Taiyo Ky", "Up by Sebastian Kamae", ... ]
  */
-function getSongDetails(songs) {}
+function getSongDetails(songs) {
+  const songDeets = songs.map((song) => `${song.title} by ${song.artist}`);
+  return songDeets;
+};
 
 /**
  * Returns an array of objects, where each object has a key that is the song title and has a value that is the song artist.
@@ -35,7 +41,14 @@ function getSongDetails(songs) {}
  *  getTitleAndArtist(songs);
  *  //> [ { "Berlin Tsukin": "Taiyo Ky" }, { Up: "Sebastian Kamae" }, ... ]
  */
-function getTitleAndArtist(songs) {}
+function getTitleAndArtist(songs) {
+  const titleAndArtistArray = songs.map((song) => { //creates new array by iterating through it and applying function to each element "song" within songs array
+  const songTitle = song.title; // for the current song object song.title processed in .map() we assign const variable songTitle
+  const songArtist = song.artist; // for the current song object song.artist processed in .map() we assign const variable songArtist
+  return {[songTitle]:songArtist}; // returning object where property key is songTitle and value songArtist. The square brackets around songTitle are used to use the value of songTitle as the key (this is called computed property name syntax).
+});
+return titleAndArtistArray; //output of getTitleAndArtist function which should return the objects made in line 48 within an array (.map() makes an array)
+};
 
 module.exports = {
   getSongTitles,
