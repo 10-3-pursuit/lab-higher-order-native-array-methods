61c5137649bc824e28ace02a010a449bbd7fda55
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-02 09:44:24 -0400

    second and third functions completed

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -26,7 +26,9 @@ function printAllSongTitles(songs) {
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  songs.forEach((element) => console.log("" + element.title + " by " + element.artist));
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
